b96fa677ce8f6ebed54cbbe2c536971ad306fe76
Laulecmor <lauren.morrison00@gmail.com>
2024-02-09 13:01:51 -0600

    Previous Edits & Adding Questions structure

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const { Circle, Square, Triangle } = require("./lib/shapes");
+
+const questions = [
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+    },
+];
